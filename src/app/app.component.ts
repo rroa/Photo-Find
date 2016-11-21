986e6edf2fdb2f7534f790978aacdd4bbf786867
Raul Roa <raul@tagshelf.com>
2016-11-21 18:53:37 -0400

    Added sidemenu implementation.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,38 @@
 import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Platform, MenuController } from 'ionic-angular';
 import { StatusBar, Splashscreen } from 'ionic-native';
 
 import { TabsPage } from '../pages/tabs/tabs';
 
 
 @Component({
-  template: `<ion-nav [root]="rootPage"></ion-nav>`
+  templateUrl: 'app.html'
 })
 export class PhotoFind {
+  authenticated: boolean = false;
   rootPage = TabsPage;
 
-  constructor(platform: Platform) {
+  constructor(platform: Platform, public menuCtrl: MenuController) {
+    // Activate menu
+    this.activateMenu();
+
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       StatusBar.styleDefault();
       Splashscreen.hide();
     });
+  }
+
+  activateMenu() {
+    this.menuCtrl.enable(this.authenticated, 'authenticated');
+    this.menuCtrl.enable(!this.authenticated, 'login');
+  }
+
+  authenticate() {
+    this.authenticated = true;
+    this.menuCtrl.close('login');    
+    this.activateMenu();
+    console.log('click', 'authenticate');
   }
 }
